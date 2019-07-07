aaacd6a5b5878f4a222d8ffb560332b6d2fd8b32
岡本尚文 <okamotonaofumi@okamo.local>
2019-07-07 22:41:43 +0900

    [Finish] 3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,27 @@ class PostsController < ApplicationController
     #ストロングパラメーターを使用
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
+
+  def index
+    @posts =Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
 
   private
   def post_params
