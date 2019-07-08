27423b6e8fdda71ac99f2550b629f8aefd460167
NaofumiOkamoto <n.okamooo@gmail.com>
2019-07-08 13:02:28 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7e97dd7ee6c7dae8fb14ca274927bbdf6ccfd9aec1a5d007528324a6ab02827b63f88f923e80cdc79db3f7b29876a54791c7548a9d15a94694545f3ef135b8da'
